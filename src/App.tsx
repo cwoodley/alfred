232d9b4d43681a267c9dab738e82e25deccee2d0
Cale Woodley <cale.woodley@wanews.com.au>
2018-07-24 12:48:07 +0800

    Creates SaveTopics component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { AlfredCache } from 'src/alfred-cache'
 import './App.css'
+import { SaveTopics } from 'src/SaveTopics'
 
 class App extends React.Component {
   private cache = new AlfredCache()
@@ -13,45 +14,66 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="wrapper">
-            <header>
-                <h1>Alfred...</h1>
-                <div className="button-group">
-                    <button type="button">5</button>
-                    <button type="button">10</button>
-                    <button type="button">15</button>
-                    <button type="button">20</button>
+          <header>
+            <h1>Alfred...</h1>
+            <div className="button-group">
+              <button type="button">5</button>
+              <button type="button">10</button>
+              <button type="button">15</button>
+              <button type="button">20</button>
+            </div>
+          </header>
+          <main>
+            <div className="sidebar">
+              <h2 className="label">Most popular</h2>
+            </div>
+            <div>
+              <div className="topic">
+                <SaveTopics topics={['sport', 'lifestyle', 'business']} />
+              </div>
+              <article>
+                <a href="http://google.com" target="_blank">
+                  <h3 className="headline">
+                    <span className="kicker">Good Job</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </h3>
+                  <p className="teaser">
+                    Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue.
+                  </p>
+                  <div className="article-data">
+                    <span className="reading-time">
+                      <span className="number">1</span>
+                      <span className="timescale">mins</span>
+                    </span>
+                    <div className="article-creation">
+                      <p className="byline">
+                        Gary Adshead | <span className="position">Political Editor</span>
+                      </p>
+                      <time dateTime="blah">Wednesday, 28th November 2016, 2:14pm</time>
+                    </div>
+                  </div>
+                </a>
+              </article>
+              <article>
+                <h3 className="headline">
+                  <span className="kicker">Good Job</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </h3>
+                <p className="teaser">
+                  Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue.
+                </p>
+                <div className="article-data">
+                  <span className="reading-time">
+                    <span className="number">1</span>
+                    <span className="timescale">mins</span>
+                  </span>
+                  <div className="article-creation">
+                    <p className="byline">
+                      Gary Adshead <span className="position">Political Editor</span>
+                    </p>
+                    <time dateTime="blah">Wednesday, 28th November 2016, 2:14pm</time>
+                  </div>
                 </div>
-            </header>
-            <main>
-                <div className="sidebar">
-                    <h2 className="label">Most popular</h2>
-                </div>
-                <div>
-                   <article>
-                       <a href="http://google.com" target="_blank">
-                        <h3 className="headline"><span className="kicker">Good Job</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-                        <p className="teaser">Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue.</p>
-                        <div className="article-data">
-                            <span className="reading-time"><span className="number">1</span><span className="timescale">mins</span></span>
-                            <div className="article-creation">
-                                <p className="byline">Gary Adshead | <span className="position">Political Editor</span></p>
-                                <time dateTime="blah">Wednesday, 28th November 2016, 2:14pm</time>
-                            </div>
-                        </div>
-                       </a>
-                    </article>
-                    <article>
-                       <h3 className="headline"><span className="kicker">Good Job</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-                       <p className="teaser">Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue.</p>
-                       <div className="article-data">
-                        <span className="reading-time"><span className="number">1</span><span className="timescale">mins</span></span>
-                        <div className="article-creation">
-                            <p className="byline">Gary Adshead <span className="position">Political Editor</span></p>
-                            <time dateTime="blah">Wednesday, 28th November 2016, 2:14pm</time>
-                        </div>
-                       </div>
-                    </article>
-                </div>
+              </article>
+            </div>
           </main>
         </div>
       </div>
---
+++ src/SaveTopics.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react'
+import * as store from 'store'
+
+type Props = {
+  topics: string[]
+}
+
+interface IState {
+  [topicName: string]: boolean
+}
+
+export class SaveTopics extends React.Component<Props, IState> {
+  constructor(props: Props) {
+    super(props)
+  }
+
+  handleChange = (event: any) => {
+    const topic = event.target
+
+    if (!topic.checked) {
+      this.setState({
+        [topic.name]: false,
+      })
+      store.remove(topic.name)
+
+      return
+    }
+
+    store.set(topic.name, true)
+
+    this.setState({
+      [topic.name]: true,
+    })
+  }
+
+  render() {
+    const topics = this.props.topics
+
+    return (
+      <div>
+        topics:
+        <form>
+          {topics &&
+            topics.map((topic, id) => {
+              return (
+                <li key={id}>
+                  {topic}
+                  <input type="checkbox" name={topic} onChange={this.handleChange} />
+                </li>
+              )
+            })}
+        </form>
+      </div>
+    )
+  }
+}
