8d6f0aee8a8ab1cd334e6e9560aa97a81339deef
Sebastian Dopierala <intelseb@gmail.com>
2018-07-24 14:08:16 +0800

    Add selected value to the button group

--- src/ButtonGroup.tsx
+++ src/ButtonGroup.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import './App.css'
 
-type Props = { onClick: any; values: number[] }
-export const ButtonGroup = ({ onClick, values }: Props) => {
+type Props = { onClick: any; values: number[]; selectedValue: number }
+
+export const ButtonGroup = ({ onClick, values, selectedValue }: Props) => {
   return (
     <div className="select-time">
       <p>How many minutes?</p>
       <div className="button-group">
         {values.map(v => (
-          <button onClick={() => onClick(v)} type="button">
+          <button className={selectedValue === v ? 'clicked' : undefined} onClick={() => onClick(v)} type="button">
             {v}
           </button>
         ))}
